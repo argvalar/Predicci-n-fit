ec31d3a83dbb7a9b3ac39644151d5a725416fd46
argvalar <araargval57@gmail.com>
2025-11-26 21:51:34 -0600

    Add files via upload

---
+++ Fitness.py
@@ -0,0 +1,58 @@
+import numpy as np
+import streamlit as st
+import pandas as pd
+from sklearn import datasets
+from sklearn.tree import DecisionTreeClassifier
+
+
+st.write(''' # Predicción de fitness de una persona ''')
+st.image("fit.jpg", caption="Dibujo que representa una vida saludable.")
+
+st.header('Datos de evaluación')
+
+def user_input_features():
+  # Entrada
+  age = st.number_input('Edad:', min_value=1, max_value=100, value = 1, step = 1)
+  height_cm = st.number_input('Altura:', min_value=0, max_value=200, value = 0, step = 1)
+  weight_kg = st.number_input('Peso:', min_value=0, max_value=200, value = 0, step = 1)
+  heart_rate = st.number_input('Frecuencia cardiaca:',min_value=0, max_value=100, value = 0, step = 1)
+  blood_pressure = st.number_input('Presión arterial:',min_value=0, max_value=200, value = 0, step = 1)
+  sleep_hours = st.number_input('Horas de sueño:', min_value=0, max_value=24, value = 0, step = 1)
+  nutrition_quality = st.number_input('Calidad de nutrición:', min_value=0, max_value=10, value = 0, step = 1)
+  activity_index = st.number_input('Indice de actividad:', min_value=0, max_value=11, value = 0, step = 1)
+  smokes = st.number_input('Fuma:', min_value=0, max_value=1, value = 0, step = 1)
+  gender= st.number_input('Género:', min_value=0, max_value=1, value = 0, step = 1)
+  user_input_data = {'age': age,
+                     'height_cm': height_cm,
+                     'weight_kg': weight_kg,
+                     'heart_rate': heart_rate,
+                     'blood_pressure': blood_pressure,
+                     'sleep_hours': sleep_hours,
+                     'nutrition_quality': nutrition_quality,
+                     'activity_index': activity_index,
+                     'smokes1': smokes,
+                     'gender': gender}
+                     
+
+  features = pd.DataFrame(user_input_data, index=[0])
+
+  return features
+
+df = user_input_features()
+
+fitness =  pd.read_csv('Fitness2.csv', encoding='latin-1')
+X = fitness.drop(columns='is_fit')
+Y = fitness['is_fit']
+
+classifier = DecisionTreeClassifier(max_depth=5, criterion='gini', min_samples_leaf=25, max_features=6, random_state=1613706)
+classifier.fit(X, Y)
+
+prediction = classifier.predict(df)
+
+st.subheader('Predicción')
+if prediction == 0:
+  st.write('No es fit')
+elif prediction == 1:
+  st.write('Sí es fit')
+else:
+  st.write('Sin predicción')
